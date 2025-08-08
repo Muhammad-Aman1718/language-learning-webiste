92c3e8afa2a12a86a3282820ee0c56a8f5f5d239
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-08 16:17:53 +0500

    Navbar and Footer responsive is completed

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,84 +1,3 @@
-// import { ChevronDown } from "lucide-react";
-// import { NavLink } from "react-router";
-// import Logo from "../../assets/logo.png";
-
-// const Navbar = () => {
-//   return (
-//     <header className="border-b  py-[28px] ">
-//       <nav className=" border w-full max-w-[1224px] mx-auto  flex justify-between items-center  ">
-//         <div>
-//           <img src={Logo} alt="Logo" />
-//         </div>
-//         <div>
-//           <ul className="flex items-center gap-x-10 text-[#404040]  ">
-//             <li>
-//               <NavLink
-//                 to="/"
-//                 className={({ isActive }) =>
-//                   isActive ? "font-bold text-[#2563EB] " : ""
-//                 }
-//               >
-//                 Home
-//               </NavLink>
-//             </li>
-//             <li>
-//               <NavLink
-//                 to="/courses"
-//                 className={({ isActive }) =>
-//                   isActive ? "font-bold text-[#2563EB] " : ""
-//                 }
-//               >
-//                 Courses
-//               </NavLink>
-//             </li>
-//             <li>
-//               <NavLink
-//                 to="/contact-us"
-//                 className={({ isActive }) =>
-//                   isActive ? "font-bold text-[#2563EB] " : ""
-//                 }
-//               >
-//                 Contact us
-//               </NavLink>
-//             </li>
-//             <li>
-//               <NavLink
-//                 to="/about-us"
-//                 className={({ isActive }) =>
-//                   isActive ? "font-bold text-[#2563EB] " : ""
-//                 }
-//               >
-//                 About us
-//               </NavLink>
-//             </li>
-//           </ul>
-//         </div>
-//         <div className=" flex items-center gap-x-6 ">
-//           {/* <User size={20} color="#404040" /> */}
-//           <NavLink
-//             to="/login"
-//             className={({ isActive }) =>
-//               `py-2 px-6 rounded-lg transition-all duration-200 ${
-//                 isActive
-//                   ? "bg-[#2563EB] text-white font-semibold"
-//                   : "border-2 border-[#2563EB] text-[#2563EB] font-medium hover:bg-[#2563EB] hover:text-white"
-//               }`
-//             }
-//           >
-//             Login
-//           </NavLink>
-//           <div className="flex items-center  gap-x-2 ">
-//             <span className=" text-[20px]  text-[#404040]  ">EN</span>
-//             <ChevronDown size={24} color="#404040" />
-//           </div>
-//         </div>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Navbar;
-
 import { ChevronDown, Menu, X } from "lucide-react";
 import { NavLink } from "react-router";
 import { useState } from "react";
@@ -180,10 +99,10 @@ const Navbar = () => {
 
         {/* Mobile Menu Overlay */}
         {isMenuOpen && (
-          <div className="lg:hidden fixed inset-0 z-50 bg-black bg-opacity-50">
-            <div className="fixed left-0 top-0 h-full w-80 bg-white shadow-lg">
+          <div className="md:hidden fixed inset-0 z-50 bg-opacity-50">
+            <div className="fixed left-0 top-0 h-full w-full bg-white shadow-lg">
               {/* Mobile Menu Header */}
-              <div className="flex items-center justify-between p-6 border-b">
+              <div className="flex items-center justify-between p-3 border-b">
                 <img src={Logo} alt="Logo" className="h-8" />
                 <button
                   onClick={toggleMenu}
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -23,18 +23,18 @@ const Login = () => {
   // };
 
   return (
-    <div className="min-h-screen flex items-center justify-center  my-[56px] ">
-      <div className="w-full max-w-[1015px]  h-[795px] rounded-[10px] grid grid-cols-1 lg:grid-cols-2 bg-[#E9EFFD] overflow-hidden">
+    <div className="flex items-center justify-center  md:my-[56px] ">
+      <div className="w-full max-w-[1015px] md:h-[795px] md:rounded-[10px]  overflow-hidden grid grid-cols-1 md:grid-cols-2 md:bg-[#E9EFFD] md:mx-7">
         {/* Left Side - Image */}
-        <div className="relative h-64 lg:h-auto">
+        <div className="relative h-64 md:h-auto">
           <div
-            className="absolute inset-0 bg-cover bg-center bg-no-repeat rounded-l-[10px]"
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat md:rounded-l-[10px]"
             style={{
               backgroundImage: `url(${AuthImg})`,
             }}
           />
-          <div className="relative z-10 p-8">
-            <h1 className="text-[20px] text-[#282828] font-medium mt-6">
+          <div className="relative z-10 p-8 max-md:p-4">
+            <h1 className="text-[20px] max-md:text-[14px] text-[#282828] font-medium  ">
               Welcome, Looks like
               <br />
               you're new here!
@@ -43,7 +43,7 @@ const Login = () => {
         </div>
 
         {/* Right Side - Login Form */}
-        <div className="  flex items-center justify-center p-6 lg:p-8">
+        <div className=" w-full max-md:max-w-[280px] mx-auto  flex items-center justify-center bg-[#E9EFFD] p-2.5 md:p-8 max-md:mt-[30px] max-md:mb-[40px] max-md:rounded-[8px]  ">
           <div className="   w-full max-w-[339px] flex flex-col gap-y-3 ">
             {/* Header */}
             <div className="">
@@ -75,7 +75,7 @@ const Login = () => {
                     // value={formData.email}
                     // onChange={handleInputChange}
                     placeholder="Enter your Email or Username"
-                    className="w-full px-4 py-3 pr-10 border bg-white border-[#BDBDBD] rounded-[8px] focus:ring-2 placeholder:text-[#BDBDBD] focus:ring-blue-500 focus:border-transparent outline-none transition-all duration-200"
+                    className="w-full px-4 max-md:px-2 py-2.5 max-md:py-1.5 pr-10 border bg-white border-[#BDBDBD] rounded-[8px] focus:ring-2 placeholder:text-[#BDBDBD] max-md:placeholder:text-[14px] focus:ring-blue-500 focus:border-transparent outline-none transition-all duration-200"
                   />
                   <Mail className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
                 </div>
@@ -91,7 +91,7 @@ const Login = () => {
                     // value={formData.password}
                     // onChange={handleInputChange}
                     placeholder="Enter your Password"
-                    className="w-full px-4 py-2.5 pr-10 border bg-white border-[#BDBDBD] rounded-[8px] focus:ring-2 placeholder:text-[#BDBDBD]  focus:ring-blue-500 focus:border-transparent outline-none transition-all duration-200"
+                    className="w-full px-4 max-md:px-2 py-2.5 max-md:py-1.5 pr-10 border bg-white border-[#BDBDBD] rounded-[8px] focus:ring-2 placeholder:text-[#BDBDBD] max-md:placeholder:text-[14px] focus:ring-blue-500 focus:border-transparent outline-none transition-all duration-200"
                   />
                   <button
                     type="button"
@@ -128,7 +128,7 @@ const Login = () => {
               {/* Login Button */}
               <button
                 // onClick={handleSubmit}
-                className="w-full bg-blue-600 text-white py-3 rounded-[8px] cursor-pointer  "
+                className="w-full bg-blue-600 text-white py-3 max-md:py-1.5 rounded-[8px] cursor-pointer  "
               >
                 Log In
               </button>
